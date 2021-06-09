ac1e332168b4206cbd83381118e2a5b9777c17bd
Mike Harris <mike.a.harris.13@gmail.com>
2021-06-08 23:44:17 -0500

    Added testing directory and an example test class
    Signed-off-by: Mike Harris <mike.a.harris.13@gmail.com>

---
+++ tests/NEAT/GeneTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2021.  Michael Harris
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * 'rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+ * AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package NEAT;
+
+class GeneTest {
+    
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+    
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void copy() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void mutateWeight() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void isEnabled() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void setEnabled() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void toggleEnabled() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void testEquals() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void getGeneID() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void setGeneID() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void getWeight() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void getFromNode() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void getToNode() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void isLargestNode() {
+    }
+    
+    @org.junit.jupiter.api.Test
+    void setLargestNode() {
+    }
+}
