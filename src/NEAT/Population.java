360c1c85bce0d75a3bf4fb12c0bc397d206c4050
Mike Harris <mike.a.harris.13@gmail.com>
2021-06-10 10:55:37 -0500

    Updates to species sorting, expansion of Population

--- src/NEAT/Population.java
+++ src/NEAT/Population.java
@@ -21,118 +21,212 @@ package NEAT;
 
 import java.util.ArrayList;
 
-public class Population {
-    private final ArrayList<Species> species;
-    private final History history;
-    private final int generationNum;
-    private final int inputs;
-    private final int outputs;
-    private ArrayList<Player> players;
-    
-    public Population() {
-        players = new ArrayList<>();
-        species = new ArrayList<>();
-        history = new History();
-        inputs = 4; // needs to change based on implementation
-        outputs = 4; //change based on implementation
-        generationNum = 0;
-    }
-    
-    public Population(int ins, int outs, int size) {
-        players = new ArrayList<>();
-        species = new ArrayList<>();
-        history = new History();
-        inputs = ins;
-        outputs = outs;
-        for (int i = 0; i < size; i++) {
-            players.add(new Player(ins, outs));
-            players.get(i).getBrain().buildNetwork();
-        }
-        generationNum = 0;
-    }
-    
-    public void updateLiving() {
-        for (Player p : players) {
-            if (p.isLiving()) {
-                p.Look();
-                p.Think();
-                p.Move();
-                p.Update();
-                p.Show();
-            }
-        }
-    }
-    
-    public boolean allDead() {
-        for (Player p : players) {
-            if (p.isLiving()) return false;
-        }
-        return true;
-    }
-    
-    
-    public void calculateFitness() {
-        for (Player p : players) {
-            p.calcFitness();
-        }
-        sortPlayers();
-    }
-    
-    private void sortPlayers() {
-        ArrayList<Player> temp = new ArrayList<>();
-        for (int i = 0; i < players.size(); i++) {
-            double max = 0;
-            int index = 0;
-            for (Player p1 : players) {
-                if (p1.getFitness() > max) {
-                    max = p1.getFitness();
-                    index = players.indexOf(p1);
-                }
-            }
-            temp.add(players.get(index));
-            players.remove(index);
-            i--;
-        }
-        players = temp;
-    }
-    
-    public void speciate() {
-        if (species.size() == 0) {
-            Species s = new Species(0, players.get(0));
-            species.add(s);
-        }
-        
-        for (Player p : players) {
-            for (Species s : species) {
-                boolean test = s.memberOfSpecies(p);
-                if (!test) {
-                    Species s1 = new Species(species.size(), p);
-                    break;
-                }
-            }
-        }
-    }
-    
-    //TODO need to add species information to each player to keep track of species during culling
-    
-    // assume pre-sorted population based on fitness
-    public void cullPop() {
-        int size = players.size();
-        while (players.size() > size / 2) {
-        
-        }
-    }
-    
-    public void repopulate(int target) {
-        while (players.size() < target) {
-        
-        }
-    }
-    
-    public History mutate(History h) {
-        for (Player p : players) {
-            h = p.mutate(h);
-        }
-        return h;
-    }
+public class Population{
+	
+	private final int inputs;
+	private final int outputs;
+	private ArrayList<Species> species;
+	private History history;
+	private int generationNum;
+	private ArrayList<Player> players;
+	
+	public Population(){
+		players = new ArrayList<>();
+		species = new ArrayList<>();
+		history = new History();
+		inputs = 4; // needs to change based on implementation
+		outputs = 4; //change based on implementation
+		for(int i = 0; i < 100; i++) {
+			players.add(new Player(inputs, outputs));
+			players.get(i).getBrain().buildNetwork();
+		}
+		generationNum = 0;
+		history = players.get(0).getBrain().buildHistory();
+		speciate();
+	}
+	
+	public void speciate(){
+		if(species.size() == 0) {
+			Species s = new Species(0, players.get(0));
+			species.add(s);
+		}
+		
+		boolean flag = false;
+		for(Player p : players) {
+			for(Species s : species) {
+				if(s.memberOfSpecies(p)) {
+					s.getSpeciesPlayer().add(p);
+					flag = true;
+					break;
+				}
+			}
+			if(!flag) {
+				Species s1 = new Species(species.size(), p);
+				species.add(s1);
+			}
+		}
+	}
+	
+	public Population(int ins, int outs, int size){
+		players = new ArrayList<>();
+		species = new ArrayList<>();
+		history = new History();
+		inputs = ins;
+		outputs = outs;
+		for(int i = 0; i < size; i++) {
+			players.add(new Player(ins, outs));
+			players.get(i).getBrain().buildNetwork();
+		}
+		generationNum = 0;
+		history = players.get(0).getBrain().buildHistory();
+		speciate();
+	}
+	
+	public void updateLiving(){
+		for(Player p : players) {
+			if(p.isLiving()) {
+				p.Look();
+				p.Think();
+				p.Move();
+				p.Update();
+				p.Show();
+			}
+		}
+	}
+	
+	public boolean allDead(){
+		for(Player p : players) {
+			if(p.isLiving()) return false;
+		}
+		return true;
+	}
+	
+	public void calculateFitness(){
+		for(Player p : players) {
+			p.calcFitness();
+		}
+		sortPlayers();
+	}
+	
+	private void sortPlayers(){
+		ArrayList<Player> temp = new ArrayList<>();
+		
+		while (players.size() > 0) {
+			double max = 0;
+			int index = 0;
+			for(Player p1 : players) {
+				if(p1.getFitness() > max) {
+					max = p1.getFitness();
+					index = players.indexOf(p1);
+				}
+			}
+			temp.add(players.get(index));
+			players.remove(index);
+		}
+		players = temp;
+	}
+	
+	public void cullAndRePop(){
+		int size;
+		Player p;
+		int start;
+		for(Species s : species) {
+			
+			s.sort();
+			size = s.getSpeciesPlayer().size();
+			if(s.getSpecAge() < 10) { // only cull the bottom 25% to allow species time to evolve
+				start = size * 3 / 4;
+			} else if(s.getSpecStale() > 15) { // if there have been no improvements, kill the whole species
+				start = 0;
+			} else { // cull the bottom 50%
+				start = size / 2;
+			}
+			for(int i = start; i < size; i++) {
+				p = s.getSpeciesPlayer().get(i);
+				s.getSpeciesPlayer().remove(p);
+				players.remove(p);
+			}
+			if(start == 0) { // remove the species from the list if it was entirely culled
+				species.remove(s);
+			} else {
+				for(int i = size / 2; i < size; i++) {
+					p = s.reproduce();
+					s.getSpeciesPlayer().add(p);
+					players.add(p);
+				}
+			}
+		}
+	}
+	
+	public void mutate(){
+		for(Player p : players) {
+			history = p.mutate(history);
+		}
+	}
+	
+	@Override
+	public int hashCode(){
+		int result = species.hashCode();
+		result = 31 * result + history.hashCode();
+		result = 31 * result + generationNum;
+		result = 31 * result + inputs;
+		result = 31 * result + outputs;
+		result = 31 * result + players.hashCode();
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if(this == o) return true;
+		if(o == null || getClass() != o.getClass()) return false;
+		
+		Population that = (Population) o;
+		
+		if(generationNum != that.generationNum) return false;
+		if(inputs != that.inputs) return false;
+		if(outputs != that.outputs) return false;
+		if(!species.equals(that.species)) return false;
+		if(!history.equals(that.history)) return false;
+		return players.equals(that.players);
+	}
+	
+	public int getInputs(){
+		return inputs;
+	}
+	
+	public int getOutputs(){
+		return outputs;
+	}
+	
+	public ArrayList<Species> getSpecies(){
+		return species;
+	}
+	
+	public void setSpecies(ArrayList<Species> species){
+		this.species = species;
+	}
+	
+	public History getHistory(){
+		return history;
+	}
+	
+	public void setHistory(History history){
+		this.history = history;
+	}
+	
+	public int getGenerationNum(){
+		return generationNum;
+	}
+	
+	public void setGenerationNum(int generationNum){
+		this.generationNum = generationNum;
+	}
+	
+	public ArrayList<Player> getPlayers(){
+		return players;
+	}
+	
+	public void setPlayers(ArrayList<Player> players){
+		this.players = players;
+	}
 }
--- src/NEAT/Species.java
+++ src/NEAT/Species.java
@@ -22,266 +22,243 @@ package NEAT;
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Species {
-    private int specID;
-    private Player reference;
-    private Player best;
-    private double bestFitness;
-    private int specAge;
-    private int specStale;
-    private ArrayList<Player> speciesPlayer;
-    
-    private double excessCoeff = 1d;
-    private double weightDiff = 0.5d;
-    private double Comp = 3d;
-    
-    public Species(int sID, Player r) {
-        specID = sID;
-        reference = r;
-        best = r;
-        bestFitness = r.getFitness();
-        speciesPlayer = new ArrayList<>();
-        speciesPlayer.add(r);
-    }
-    
-    public Species(int sID) {
-        specID = sID;
-        reference = null;
-        best = null;
-        bestFitness = 0;
-        speciesPlayer = new ArrayList<>();
-    }
-    
-    public boolean memberOfSpecies(Player p) {
-        double excess = getExcess(p, reference);
-        double wDiff = getWDiff(p, reference);
-        int normalizer = p.getBrain().getGenes().size() - 20;
-        if (normalizer < 1) normalizer = 1;
-        double compat = excessCoeff * excess / normalizer + weightDiff * wDiff;
-        if (compat < Comp) {
-            speciesPlayer.add(p);
-            return true;
-        } else {
-            return false;
-        }
-    }
-    
-    private double getExcess(Player p, Player r) {
-        Genome pg = p.getBrain();
-        Genome rg = r.getBrain();
-        double excess = 0d;
-        int temp;
-        for (Gene g : rg.getGenes()) {
-            temp = pg.matchingGene(g);
-            if (temp == -1) excess++;
-        }
-        return excess;
-    }
-    
-    private double getWDiff(Player p, Player r) {
-        Genome pg = p.getBrain();
-        Genome rg = r.getBrain();
-        double diff = 0;
-        double matches = 0;
-        for (Gene g : pg.getGenes()) {
-            for (Gene g1 : rg.getGenes()) {
-                if (g.getGeneID() == g1.getGeneID()) {
-                    matches++;
-                    diff += Math.abs(g.getWeight() - g1.getWeight());
-                    break;
-                }
-            }
-        }
-        if (matches == 0) {
-            return 1000;
-        } else {
-            return diff / matches;
-        }
-    }
-    
-    public Player reproduce(History h) {
-        Player a = selectPlayer();
-        Player b = selectPlayer();
-        Player p;
-        if (a.getFitness() > b.getFitness()) {
-            p = a.crossOver(b);
-        } else {
-            p = b.crossOver(a);
-        }
-        return p;
-    }
-    
-    public Player selectPlayer() {
-        fitnessSumScale();
-        double rand = ThreadLocalRandom.current().nextDouble();
-        for (Player p : speciesPlayer) {
-            if (rand < p.getCumFit()) return p;
-        }
-        return null;
-    }
-    
-    private void fitnessSumScale() {
-        sort();
-        double f = 0;
-        for (Player p : speciesPlayer) {
-            f += p.getFitness();
-        }
-        for (Player p : speciesPlayer) {
-            p.setScaleFit(p.getFitness() / f);
-        }
-        
-        speciesPlayer.get(0).setCumFit(speciesPlayer.get(0).getScaleFit());
-        for (int i = 1; i < speciesPlayer.size(); i++) {
-            speciesPlayer.get(i).setCumFit(speciesPlayer.get(i - 1).getCumFit() + speciesPlayer.get(i).getScaleFit());
-        }
-    }
-    
-    public void sort() {
-        ArrayList<Player> temp = new ArrayList<>();
-        for (int i = 0; i < speciesPlayer.size(); i++) {
-            double max = 0;
-            int index = 0;
-            for (Player p1 : speciesPlayer) {
-                if (p1.getFitness() > max) {
-                    max = p1.getFitness();
-                    index = speciesPlayer.indexOf(p1);
-                }
-            }
-            temp.add(speciesPlayer.get(index));
-            speciesPlayer.remove(index);
-            i--;
-        }
-        speciesPlayer = temp;
-        determineBest();
-    }
-    
-    private void determineBest() {
-        if (speciesPlayer.get(0).getFitness() > bestFitness) {
-            specStale = 0;
-            best = speciesPlayer.get(0);
-            bestFitness = speciesPlayer.get(0).getFitness();
-        } else {
-            specStale++;
-        }
-    }
-    
-    @Override
-    public int hashCode() {
-        int result;
-        long temp;
-        result = getSpecID();
-        result = 31 * result + getReference().hashCode();
-        result = 31 * result + getBest().hashCode();
-        temp = Double.doubleToLongBits(getBestFitness());
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        result = 31 * result + getSpecAge();
-        result = 31 * result + getSpecStale();
-        result = 31 * result + (getSpeciesPlayer() != null ? getSpeciesPlayer().hashCode() : 0);
-        temp = Double.doubleToLongBits(getExcessCoeff());
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(getWeightDiff());
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        temp = Double.doubleToLongBits(getComp());
-        result = 31 * result + (int) (temp ^ (temp >>> 32));
-        return result;
-    }
-    
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        
-        Species species = (Species) o;
-        
-        if (getSpecID() != species.getSpecID()) return false;
-        if (Double.compare(species.getBestFitness(), getBestFitness()) != 0) return false;
-        if (getSpecAge() != species.getSpecAge()) return false;
-        if (getSpecStale() != species.getSpecStale()) return false;
-        if (Double.compare(species.getExcessCoeff(), getExcessCoeff()) != 0) return false;
-        if (Double.compare(species.getWeightDiff(), getWeightDiff()) != 0) return false;
-        if (Double.compare(species.getComp(), getComp()) != 0) return false;
-        if (!getReference().equals(species.getReference())) return false;
-        if (!getBest().equals(species.getBest())) return false;
-        return getSpeciesPlayer() != null ? getSpeciesPlayer().equals(species.getSpeciesPlayer()) : species.getSpeciesPlayer() == null;
-    }
-    
-    public int getSpecID() {
-        return specID;
-    }
-    
-    public void setSpecID(int specID) {
-        this.specID = specID;
-    }
-    
-    public Player getReference() {
-        return reference;
-    }
-    
-    public void setReference(Player reference) {
-        this.reference = reference;
-    }
-    
-    public Player getBest() {
-        return best;
-    }
-    
-    public void setBest(Player best) {
-        this.best = best;
-    }
-    
-    public double getBestFitness() {
-        return bestFitness;
-    }
-    
-    public void setBestFitness(double bestFitness) {
-        this.bestFitness = bestFitness;
-    }
-    
-    public int getSpecAge() {
-        return specAge;
-    }
-    
-    public void setSpecAge(int specAge) {
-        this.specAge = specAge;
-    }
-    
-    public int getSpecStale() {
-        return specStale;
-    }
-    
-    public void setSpecStale(int specStale) {
-        this.specStale = specStale;
-    }
-    
-    public ArrayList<Player> getSpeciesPlayer() {
-        return speciesPlayer;
-    }
-    
-    public void setSpeciesPlayer(ArrayList<Player> speciesPlayer) {
-        this.speciesPlayer = speciesPlayer;
-    }
-    
-    public double getExcessCoeff() {
-        return excessCoeff;
-    }
-    
-    public void setExcessCoeff(double excessCoeff) {
-        this.excessCoeff = excessCoeff;
-    }
-    
-    public double getWeightDiff() {
-        return weightDiff;
-    }
-    
-    public void setWeightDiff(double weightDiff) {
-        this.weightDiff = weightDiff;
-    }
-    
-    public double getComp() {
-        return Comp;
-    }
-    
-    public void setComp(double comp) {
-        Comp = comp;
-    }
+public class Species{
+	
+	private int specID;
+	private Player reference;
+	private Player best;
+	private double bestFitness;
+	private int specAge;
+	private int specStale;
+	private ArrayList<Player> speciesPlayer;
+	
+	private final double excessCoeff = 1d;
+	private final double weightDiff = 0.5d;
+	private final double Comp = 3d;
+	
+	public Species(int sID, Player r){
+		specID = sID;
+		reference = r;
+		best = r;
+		bestFitness = r.getFitness();
+		speciesPlayer = new ArrayList<>();
+		speciesPlayer.add(r);
+	}
+	
+	public Species(int sID){
+		specID = sID;
+		reference = null;
+		best = null;
+		bestFitness = 0;
+		speciesPlayer = new ArrayList<>();
+	}
+	
+	public boolean memberOfSpecies(Player p){
+		double excess = getExcess(p, reference);
+		double wDiff = getWDiff(p, reference);
+		int normalizer = p.getBrain().getGenes().size() - 20;
+		if(normalizer < 1) normalizer = 1;
+		double compat = excessCoeff * excess / normalizer + weightDiff * wDiff;
+		if(compat < Comp) {
+			speciesPlayer.add(p);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private double getExcess(Player p, Player r){
+		Genome pg = p.getBrain();
+		Genome rg = r.getBrain();
+		double excess = 0d;
+		int temp;
+		for(Gene g : rg.getGenes()) {
+			temp = pg.matchingGene(g);
+			if(temp == -1) excess++;
+		}
+		return excess;
+	}
+	
+	private double getWDiff(Player p, Player r){
+		Genome pg = p.getBrain();
+		Genome rg = r.getBrain();
+		double diff = 0;
+		double matches = 0;
+		for(Gene g : pg.getGenes()) {
+			for(Gene g1 : rg.getGenes()) {
+				if(g.getGeneID() == g1.getGeneID()) {
+					matches++;
+					diff += Math.abs(g.getWeight() - g1.getWeight());
+					break;
+				}
+			}
+		}
+		if(matches == 0) {
+			return 1000;
+		} else {
+			return diff / matches;
+		}
+	}
+	
+	public Player reproduce(){
+		Player a = selectPlayer();
+		Player b = selectPlayer();
+		Player p;
+		if(a.getFitness() > b.getFitness()) {
+			p = a.crossOver(b);
+		} else {
+			p = b.crossOver(a);
+		}
+		return p;
+	}
+	
+	public Player selectPlayer(){
+		double rand = ThreadLocalRandom.current().nextDouble();
+		int index = ThreadLocalRandom.current().nextInt(speciesPlayer.size());
+		double temp = 0.07d * Math.exp(-0.07d * index);
+		int counter = 0;
+		while (temp < rand && counter < speciesPlayer.size() * 10) {
+			index = ThreadLocalRandom.current().nextInt(speciesPlayer.size());
+			temp = 0.07d * Math.exp(-0.07d * index);
+			counter++;
+		}
+		return speciesPlayer.get(index);
+	}
+	
+	public void sort(){
+		ArrayList<Player> temp = new ArrayList<>();
+		
+		while (speciesPlayer.size() > 0) {
+			double max = 0;
+			int index = 0;
+			for(Player p1 : speciesPlayer) {
+				if(p1.getFitness() > max) {
+					max = p1.getFitness();
+					index = speciesPlayer.indexOf(p1);
+				}
+			}
+			temp.add(speciesPlayer.get(index));
+			speciesPlayer.remove(index);
+		}
+		speciesPlayer = temp;
+		determineBest();
+	}
+	
+	private void determineBest(){
+		if(speciesPlayer.get(0).getFitness() > bestFitness) {
+			specStale = 0;
+			best = speciesPlayer.get(0);
+			bestFitness = speciesPlayer.get(0).getFitness();
+		} else {
+			specStale++;
+		}
+	}
+	
+	@Override
+	public int hashCode(){
+		int result;
+		long temp;
+		result = getSpecID();
+		result = 31 * result + getReference().hashCode();
+		result = 31 * result + getBest().hashCode();
+		temp = Double.doubleToLongBits(getBestFitness());
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		result = 31 * result + getSpecAge();
+		result = 31 * result + getSpecStale();
+		result = 31 * result + (getSpeciesPlayer() != null ? getSpeciesPlayer().hashCode() : 0);
+		temp = Double.doubleToLongBits(getExcessCoeff());
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(getWeightDiff());
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(getComp());
+		result = 31 * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if(this == o) return true;
+		if(o == null || getClass() != o.getClass()) return false;
+		
+		Species species = (Species) o;
+		
+		if(getSpecID() != species.getSpecID()) return false;
+		if(Double.compare(species.getBestFitness(), getBestFitness()) != 0) return false;
+		if(getSpecAge() != species.getSpecAge()) return false;
+		if(getSpecStale() != species.getSpecStale()) return false;
+		if(Double.compare(species.getExcessCoeff(), getExcessCoeff()) != 0) return false;
+		if(Double.compare(species.getWeightDiff(), getWeightDiff()) != 0) return false;
+		if(Double.compare(species.getComp(), getComp()) != 0) return false;
+		if(!getReference().equals(species.getReference())) return false;
+		if(!getBest().equals(species.getBest())) return false;
+		return getSpeciesPlayer() != null ? getSpeciesPlayer().equals(species.getSpeciesPlayer()) : species.getSpeciesPlayer() == null;
+	}
+	
+	public int getSpecID(){
+		return specID;
+	}
+	
+	public void setSpecID(int specID){
+		this.specID = specID;
+	}
+	
+	public Player getReference(){
+		return reference;
+	}
+	
+	public void setReference(Player reference){
+		this.reference = reference;
+	}
+	
+	public Player getBest(){
+		return best;
+	}
+	
+	public void setBest(Player best){
+		this.best = best;
+	}
+	
+	public double getBestFitness(){
+		return bestFitness;
+	}
+	
+	public void setBestFitness(double bestFitness){
+		this.bestFitness = bestFitness;
+	}
+	
+	public int getSpecAge(){
+		return specAge;
+	}
+	
+	public void setSpecAge(int specAge){
+		this.specAge = specAge;
+	}
+	
+	public int getSpecStale(){
+		return specStale;
+	}
+	
+	public void setSpecStale(int specStale){
+		this.specStale = specStale;
+	}
+	
+	public ArrayList<Player> getSpeciesPlayer(){
+		return speciesPlayer;
+	}
+	
+	public void setSpeciesPlayer(ArrayList<Player> speciesPlayer){
+		this.speciesPlayer = speciesPlayer;
+	}
+	
+	public double getExcessCoeff(){
+		return excessCoeff;
+	}
+	
+	public double getWeightDiff(){
+		return weightDiff;
+	}
+	
+	public double getComp(){
+		return Comp;
+	}
 }
